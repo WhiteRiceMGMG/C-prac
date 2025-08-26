dcde02ddb28e3df19c56ae6d0a4662cce2d951c5
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-26 23:15:57 +0900

    a

--- prj1/std8.cpp
+++ prj1/std8.cpp
@@ -11,5 +11,31 @@ int main(void)
     double d = 2568.2;
     long double ld = 1.258E-25;
     size_t sz;
-    
+
+    sz = sizeof c;
+    cout << "size of char = " << sz << "バイト" << endl;
+
+    sz = sizeof s;
+    cout << "size of short = " << sz << "バイト" << endl;
+
+    sz = sizeof i;
+    cout << "size of int = " << sz << "バイト" << endl;
+
+    sz = sizeof ui;
+    cout << "size of unsigned int = " << sz << "バイト" << endl;
+
+    sz = sizeof f;
+    cout << "size of float = " << sz << "バイト" << endl;
+
+    sz = sizeof d;
+    cout << "size of double = " << sz << "バイト" << endl;
+
+    sz = sizeof ld;
+    cout << "size of long double = " << sz << "バイト" << endl;
+
+    sz = sizeof(size_t);
+    cout << "size of sz = " << sz << "バイト" << endl;
+
+    return 0;
+
 }
