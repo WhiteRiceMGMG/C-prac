209d30655357862f8fb1306954fe776c29736f4a
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-26 23:02:48 +0900

    a

--- prj1/std6.cpp
+++ prj1/std6.cpp
@@ -6,11 +6,12 @@ int main(void)
     int a = 10;
     int b = 3;
     double c;
+    // 同じ型で割ってるのでその型になる．
     c = a / b;
 
     cout << a << " ÷ " << b << "=" << c << endl;
 
     return 0;
-    
+
 
 }
