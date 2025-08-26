36ef670841d94ba35ec258c8f3314a160ded802f
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-26 22:46:32 +0900

    a

--- prj1/std5.cpp
+++ prj1/std5.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 
 using namespace std;
+
+int main(void)
+{
+    int x;
+    int y;
+
+    cout << "x yを入力してください" ;
+    cin >> x >> y;
+    cout << "x=" << x << y << "y" << endl;
+
+    return 0;
+    
+}
