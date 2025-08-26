3ccfe197e86b83991a36bc46cf16088113ac8c81
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-26 23:11:24 +0900

    a

---
+++ prj1/std8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    char c = 'A';
+    short s = 50;
+    int i = -100;
+    unsigned int ui = 128;
+    float f = 0.5f;
+    double d = 2568.2;
+    long double ld = 1.258E-25;
+    size_t sz;
+    
+}
