0d76b55ee3c2c8f572768956e3fa6dcda2ba386c
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-26 22:58:35 +0900

    a

---
+++ prj1/std6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = 10;
+    int b = 3;
+    double c;
+    c = a / b;
+
+    cout << a << " ÷ " << b << "=" << c << endl;
+
+    return 0;
+    
+
+}
