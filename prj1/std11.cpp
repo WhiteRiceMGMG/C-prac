8d8a133ea40c6ab8437dfd284b873c384564f4e4
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-27 23:09:26 +0900

    a

--- prj1/std11.cpp
+++ prj1/std11.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+int main(void)
+{
+    int a1, a2, a3, a4;
+    a1 = 5;
+    a1 = a1 + 2;
+    cout << "a1 = " << a1 << endl;
+
+    a2 = 5;
+    a2 += 5;
+    cout << "a2 = " << a2 << endl;
+    
+}
