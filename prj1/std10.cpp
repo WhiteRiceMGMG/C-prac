d44157e15cdb646b4f130d1028fcb93ec414c9fa
WhiteRiceMGMG <3223mq118@gmail.com>
2025-08-27 21:53:26 +0900

    a

--- prj1/std10.cpp
+++ prj1/std10.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main(void)
 {
-    
+    int a = 10;
+    cout << "aの値は" << a << "です" << endl;
+    cout << "a == 10の値は" << (a == 10) << "です" << endl;
+    cout << "a == 20の値は" << (a == 20) << "です" << endl;
 }
