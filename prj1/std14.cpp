93cac2c6bff79d6794cf62a912bbdcebb6128542
ogawa-vaio <3223mq118@gmail.com>
2025-08-31 22:18:10 +0900

    a

--- prj1/std14.cpp
+++ prj1/std14.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 
 using namespace std;
+
+int main(void)
+{
+    int age;
+    cout << "この度は猫でもランドに" << endl;
+    cout << "ご来園ありがとうございます．" << endl;
+
+    cout << "年齢を入力してください．" << endl;
+    cin >> age;
+
+    if(age < 6)
+    {
+        cout << "入場料は無料です．" << endl;
+    }
+    
+}
