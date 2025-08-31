0e014f5aa6cfebcead3fe13bcbd55f8f493520ca
ogawa-vaio <3223mq118@gmail.com>
2025-08-31 22:35:27 +0900

    a

--- prj1/std16.cpp
+++ prj1/std16.cpp
@@ -6,5 +6,33 @@ basic _istrem& getline(
 )
  *****************************************************************/
 
+#include <iostream>
+using namespace std;
 
- 
+int main(void)
+{
+    int c;
+    char buf[16];
+    bool end = false;
+
+    while(1)
+    {
+        cout << "----MENU----" << endl;
+        cout << "A Game" << endl;
+        cout << "B Music" << endl;
+        cout << "C table" << endl;
+        cout << "X end" << endl;
+        cout << "------------" < endl;
+
+        while(1)
+        {
+            cin.getline(buf, 16);
+            c = buf[0];
+            if (c != '\n')
+            {
+                break;
+            }
+        }
+        
+    }
+}
